d32587a665c977e63623ad2a40028345b4154bc1
Ismo Lundbom <ismo.lundbom@gmail.com>
2018-10-15 17:21:21 +0300

    Assignment finished

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,8 +1,48 @@
+import java.util.*;
 
 public class RomanNumerals {
 	public int convertToInteger(String romanNum) {
-		// To be Implemented
-		return 0;
 		
+		//if the length is 0 or string is empty, return 0
+		if (romanNum == null || romanNum.length() == 0) {
+			return 0;
+		}
+		
+		//if romanNum isn't a valid roman numeral return -1
+		if (!validateRoman(romanNum)) {
+			return -1;
+		}
+		
+		//hashmap for roman numerals and their values
+		Map<Character, Integer> romanValues = new HashMap<>();
+		romanValues.put('I',1);
+		romanValues.put('V',5);
+		romanValues.put('X',10);
+		romanValues.put('L',50);
+		romanValues.put('C',100);
+		romanValues.put('D',500);
+		romanValues.put('M',1000);
+		
+		int length = romanNum.length();
+		int result = romanValues.get(romanNum.charAt(length - 1));
+		
+		for (int i = length - 2; i >= 0; i--) {
+			if (romanValues.get(romanNum.charAt(i)) >= romanValues.get(romanNum.charAt(i + 1))) {
+				result += romanValues.get(romanNum.charAt(i));
+			}else {
+				result -= romanValues.get(romanNum.charAt(i));
+			}
+		}		
+		return result;		
+	}
+	
+	//here we check, if the romanNum is a valid Roman numeral
+	public boolean validateRoman(String romanNum) {
+		boolean valid = romanNum.matches("^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$");
+		if (!valid) {
+			System.out.println("Not a valid roman numeral: " + romanNum);
+		}		
+		return valid;
 	}
 }
+
--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -9,4 +9,122 @@ public class TestRomanNumerals {
 		fail("Not yet implemented");
 	}
 
+	@Test
+	public void test_1() {
+		RomanNumerals rm = new RomanNumerals();
+		int x = 0;
+		x = rm.convertToInteger("V");
+		assertEquals("Should be 5",5,x);
+	}
+	
+	@Test
+	public void test_2() {
+		RomanNumerals rm = new RomanNumerals();
+		int x = 0;
+		x = rm.convertToInteger("X");
+		assertEquals("Should be 10",10,x);
+	}
+	
+	@Test
+	public void test_3() {
+		RomanNumerals rm = new RomanNumerals();
+		int x = 0;
+		x = rm.convertToInteger("IX");
+		assertEquals("Should be 9",9,x);
+	}
+	
+	@Test
+	public void test_4() {
+		RomanNumerals rm = new RomanNumerals();
+		int x = 0;
+		x = rm.convertToInteger("III");
+		assertEquals("Should be 3",3,x);
+	}
+	
+	@Test
+	public void test_5() {
+		RomanNumerals rm = new RomanNumerals();
+		int x = 0;
+		x = rm.convertToInteger("XI");	
+		assertEquals("11",11,x);
+	}
+	
+	@Test
+	public void test_6() {
+		RomanNumerals rm = new RomanNumerals();
+		int x = 0;
+		x = rm.convertToInteger("MDI");	
+		assertEquals("1501",1501,x);
+	}
+	
+	@Test
+	public void test_7() {
+		RomanNumerals rm = new RomanNumerals();
+		int x = 0;
+		x = rm.convertToInteger("VV");	
+		assertEquals("Shouldn't be valid",10,x);
+	}
+	
+	@Test
+	public void test_8() {
+		RomanNumerals rm = new RomanNumerals();
+		int x = 0;
+		x = rm.convertToInteger("VVV");	
+		assertEquals("Shouldn't be valid",10,x);
+	}	
+	
+	@Test
+	public void test_9() {
+		RomanNumerals rm = new RomanNumerals();
+		int x = 0;
+		x = rm.convertToInteger("XXX");	
+		assertEquals("Should be 30",30,x);
+	}	
+	
+	@Test
+	public void test_10() {
+		RomanNumerals rm = new RomanNumerals();
+		boolean x = true;
+		x = rm.validateRoman("XXX");	
+		assertEquals("Should be true",true,x);
+	}
+	
+	@Test
+	public void test_11() {
+		RomanNumerals rm = new RomanNumerals();
+		boolean x = true;
+		x = rm.validateRoman("IIII");	
+		assertEquals("Should be false",false,x);
+	}
+	
+	@Test
+	public void test_12() {
+		RomanNumerals rm = new RomanNumerals();
+		boolean x = true;
+		x = rm.validateRoman("XXXXX");	
+		assertEquals("Should be false",false,x);
+	}
+	@Test
+	public void test_13() {
+		RomanNumerals rm = new RomanNumerals();
+		boolean x = true;
+		x = rm.validateRoman("DD");	
+		assertEquals("Should be false",false,x);
+	}
+	
+	@Test
+	public void test_14() {
+		RomanNumerals rm = new RomanNumerals();
+		boolean x = true;
+		x = rm.validateRoman("XV");	
+		assertEquals("Should be true",true,x);
+	}
+	
+	@Test
+	public void test_15() {
+		RomanNumerals rm = new RomanNumerals();
+		boolean x = true;
+		x = rm.validateRoman("MMM");	
+		assertEquals("Should be true",true,x);
+	}	
 }
